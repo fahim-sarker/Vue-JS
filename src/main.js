7b5804b936ed77315a3753ad3dbf166e45e2e70e
fahim-sarker <sarkerfahim599@gmail.com>
2025-07-05 11:53:02 +0600

    change

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import { createApp } from "vue"; 
+import { createApp } from "vue";
 import App from "./App.vue";
-import "../src/style.css"
+import "../src/style.css";
 import router from "./router/Index";
 
 createApp(App).use(router).mount("#app");
